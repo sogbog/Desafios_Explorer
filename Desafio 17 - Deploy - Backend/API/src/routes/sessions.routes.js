e262356eaa7dc543499fdd4c646f20ce1aa5f292
Andre Luiz Gomes Sampaio <100450627+sogbog@users.noreply.github.com>
2023-01-23 19:35:11 -0300

    Delete Desafio 17 - Deploy - Backend/API directory

--- Desafio 17 - Deploy - Backend/API/src/routes/sessions.routes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-const SessionController = require("../controllers/session.controller")
-
-const sessionController = new SessionController()
-
-const sessionRoutes = Router()
-sessionRoutes.post("/", sessionController.create)
-
-module.exports = sessionRoutes
